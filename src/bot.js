af2f0ef4b736bcdc74361a8ff1f75f815eecba79
Matthew Loughney <mloughne@amazon.com>
2020-12-09 15:40:00 -0800

    Added string similarity search and response

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,11 @@
-const tmi = require('tmi.js');
+import tmi from 'tmi.js';
+import dljs from 'damerau-levenshtein-js';
+
+export const questionsAndAnswers = {
+  "Why is the game in Japanese?": "Japanese text is faster, and games are generally released earlier in Japan, so there are more exploitable bugs that are patched in other versions."
+}
+
+export const THRESHOLD = 10;
 
 // Define configuration options
 const opts = {
@@ -22,31 +29,37 @@ client.on('connected', onConnectedHandler);
 client.connect();
 
 // Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
+function onMessageHandler(target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
 
   // Remove whitespace from chat message
   const commandName = msg.trim();
 
+  for (const question of Object.keys(questionsAndAnswers)) {
+    console.log(dljs.distance(question, msg));
+    console.log(questionsAndAnswers[question])
+    if (dljs.distance(question, msg) < THRESHOLD) {
+      // client.say(target, questionsAndAnswers[question]);
+    }
+  }
   // If the command is known, let's execute it
   if (commandName === '!dice') {
     const num = rollDice();
     client.say(target, `You rolled a ${num}`);
+    client
     console.log(`* Executed ${commandName} command`);
-  } else if (commandName === 'asdf') {
-    client.say(target, `You said asdf`);
   } else {
     console.log(`* Unknown command ${commandName}`);
   }
 }
 
 // Function called when the "dice" command is issued
-function rollDice () {
+function rollDice() {
   const sides = 6;
   return Math.floor(Math.random() * sides) + 1;
 }
 
 // Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
+function onConnectedHandler(addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
 }
